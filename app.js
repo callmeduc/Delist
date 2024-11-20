6e8fc26a7e7e1be84118924ced0936d9779177f0
Duc Duong <62574717+callmeduc@users.noreply.github.com>
2024-11-20 16:12:16 +0700

    fix eslint

--- app.js
+++ app.js
@@ -12,11 +12,11 @@ app.use(express.json());
 
 checkDelist();
 
+
+
 // Routes
 app.use("/", userRoutes);
 
-const __dirname = path.resolve();
-app.use("/uploads", express.static(path.join(__dirname, "/uploads")));
 
 app.use(notFound);
 app.use(errorHandler);
