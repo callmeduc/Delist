3db61975655cfd716872cd3ce0a071cdc0bf6e91
Duc Duong <62574717+callmeduc@users.noreply.github.com>
2024-11-26 08:25:31 +0700

    delist optimise

--- delist.js
+++ delist.js
@@ -17,7 +17,8 @@ let browser;
 let previousTitle = "";
 
 async function initBrowser() {
-  if (!browser) {
+  if (!browser || !browser.isConnected()) {
+    console.log("Khởi chạy trình duyệt...");
     browser = await chromium.launch({ headless: true });
   }
   return browser;
@@ -25,7 +26,6 @@ async function initBrowser() {
 
 async function checkAnnouncement(page) {
   try {
-    // Chặn các tài nguyên không cần thiết
     await page.route("**/*", (route) => {
       const resourceType = route.request().resourceType();
       if (["stylesheet", "image", "font"].includes(resourceType)) {
@@ -36,21 +36,18 @@ async function checkAnnouncement(page) {
     });
 
     await page.goto(ANNOUNCE_URL, {
-      waitUntil: "domcontentloaded", // Chỉ chờ DOMContentLoaded
+      waitUntil: "domcontentloaded",
       timeout: 30000,
     });
 
-    const firstChildDivText = await page
+    const announcement = await page
       .locator(".css-1q4wrpt > .css-1tl1y3y")
-      .first()
-      .innerText();
-
-    const date = firstChildDivText.slice(-10).trim();
+      .first();
+    const firstChildDivText = await announcement.textContent();
     const title = firstChildDivText.slice(0, -10).trim();
 
     if (title !== previousTitle) {
-      console.log("Text: ", firstChildDivText);
-      console.log("Tiêu đề mới:", title);
+      console.log("Thông báo mới:", title);
       const now = new Date().toLocaleString("en-US", {
         timeZone: "Asia/Ho_Chi_Minh",
         hour12: false,
@@ -63,32 +60,36 @@ async function checkAnnouncement(page) {
       await writeToDoc([text], DOC_DELIST_ID);
       previousTitle = title;
     } else {
-      console.log("Không có thông báo mới");
+      console.log("Không có thông báo mới.");
     }
   } catch (error) {
-    console.error("Lỗi khi lấy thông báo:", error);
+    console.error("Lỗi khi kiểm tra thông báo:", error);
   }
 }
 
 export async function handleDelist() {
+  let page;
   try {
     const response = await axios.get(REQUEST_URL);
-    console.log("PET - Request OK: ", response.data);
+    console.log("PET - Request OK:", response.data);
 
-    const page = await (await initBrowser()).newPage();
+    page = await (await initBrowser()).newPage();
     await checkAnnouncement(page);
-    await page.close();
   } catch (error) {
     console.error("Lỗi trong quá trình kiểm tra thông báo:", error);
+  } finally {
+    if (page) await page.close();
   }
 }
 
+// Lên lịch với cron
 cron.schedule(CRON_SCHEDULE, handleDelist);
 cron.schedule(CRON_MINUTE_FIX, handleDelist);
 
+// Đảm bảo đóng trình duyệt khi ứng dụng thoát
 process.on("exit", async () => {
   if (browser) {
     await browser.close();
-    console.log("Trình duyệt đã đóng khi thoát.");
+    console.log("Trình duyệt đã được đóng.");
   }
 });
